c64eed56747e64474e10ed2455e327fe93c29c4a
Aqua430 <megenislam@gmail.com>
2025-08-11 15:49:44 +0500

    Changed fetch resource to railway server via backend

--- currency-converter_frontend/js/main.js
+++ currency-converter_frontend/js/main.js
@@ -22,7 +22,7 @@ function checkForm(event){
 }
 
 function convert(amount, from, to) {
-    fetch(`https://currency-converter-production-2995.up.railway.app//convert?from=${from}&to=${to}&amount=${amount}`)
+    fetch(`https://currency-converter-production-2995.up.railway.app/convert?from=${from}&to=${to}&amount=${amount}`)
         .then(response => response.json())
         .then(data => {
             document.getElementById("result").textContent = data.result.toFixed(3);
